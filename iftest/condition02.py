8ee39f16a1a55522fd9ed22c8890c468d716a4b9
ghafeez <ghafeez@users.noreply.github.com>
2022-01-25 22:29:54 +0000

    working on if, elif, else logic

--- iftest/condition02.py
+++ iftest/condition02.py
@@ -3,3 +3,12 @@ hostname = input("What value should we set for hostname?")
 if hostname == "MTG":
     print("The hostname was found to be MTG")
 
+Monday = input("enter the score secured on Monday")
+Tuesday = input("enter the score secured on Tuesday")
+Wednesday = input("enter the score secured on Wednesday")
+if Monday >= "20"
+    print("you are over")
+else print("you are fine")
+if Tuesday >= 50
+    print("you are over")
+else print("you are fine")
