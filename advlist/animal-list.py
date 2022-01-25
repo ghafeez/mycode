7d7cace2b370e8b9be474274ac562dbeb2c645e1
ghafeez <ghafeez@users.noreply.github.com>
2022-01-25 16:14:48 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""ghafeez 
+    printing lists"""
+    
+
+def main():
+    
+    #printing a list of three letter named animals
+    list3=["ant", "cat", "dog", "hog", "bee"]
+    print(list3)
+
+main()
+
