a6fb5da92be2354f9ea34c8652b2f759f6754e02
ghafeez <ghafeez@users.noreply.github.com>
2022-01-25 21:42:23 +0000

    Learning about conditional if

---
+++ iftest/condition03.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+hostname = input("What value should we set for hostname?") # asking user's input
+
+if hostname.lower() == "mtg":                       #defining the condition and lowering the case
+    print("The hostname was found to be mtg")       #statement if input value matches condition
+    print ("hostname matches expected config")      #printing matching statement
+    print("exiting the script")                     #printing the exiting of script statement
+
