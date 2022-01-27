0aba785b482eb341f3984c398c64f1db44b56f4b
ghafeez <ghafeez@users.noreply.github.com>
2022-01-27 19:50:13 +0000

    Python and physical interfaces

---
+++ challenge02-poke.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Alta3 Research | RZFeeser
+   For Loop Example - Create for loop to work across the dataset provided"""
+
+print("Welcome to the PokeAPI data parser")
+
+# dataSet is JSON adapted from https://PokeAPI.co
+dataSet = [{"ability": {"name": "static", "attack type": "electric"}, "is_hidden": False, "slot":1}, {"ability": {"name": "lightning-rod", "attack type": "electric"}, "is_hidden": True, "slot": 3}]
+
+# Finish the code below this line
+# -------------------------------
+
+
+# use a for loop to display all abilities
+for i in dataSet:
+    print(dataSet[i])
+
+for i in dataSet():
+    print(dataSet.values())
+
+# use a for loop to display JUST the details of a single ability
+
+
+# use a for loop to display JUST the names of a single ability
+
